3d681532e8784c4dd6196ef63afe8b6835d1cdfe
JonathanSimon098 <jonedu1130@gmail.com>
2024-04-20 01:10:16 -0500

    Updated switch config

--- ECE319K_Lab9H/Switch.cpp
+++ ECE319K_Lab9H/Switch.cpp
@@ -14,11 +14,11 @@
 #define DOWN_BTN PB13INDEX // PB13
 #define JOYSTICK_BTN PA24INDEX // PA24
 // update if different inputs ^ v
-#define LEFT_INDEX (1<<16)
-#define UP_INDEX (1<<17)
-#define RIGHT_INDEX (1<<12)
-#define DOWN_INDEX (1<<13)
-#define JOYSTICK_INDEX (1<<24)
+#define LEFT_INDEX 16
+#define UP_INDEX 17
+#define RIGHT_INDEX 12
+#define DOWN_INDEX 13
+#define JOYSTICK_INDEX 24
 
 // LaunchPad.h defines all the indices into the PINCM table
 void Switch_Init(void){
@@ -33,34 +33,35 @@ void Switch_Init(void){
 // return current state of left switch
 uint32_t JOYSTICK_Switch_In(void){
     // write this
-    return (GPIOA->DIN31_0)&JOYSTICK_INDEX;
+    return (GPIOA->DIN31_0)&(1<<JOYSTICK_INDEX);
 }
 
 // return current state of left switch
 uint32_t LEFT_Switch_In(void){
     // write this
-    return (GPIOB->DIN31_0)&LEFT_INDEX;
+    return ((GPIOB->DIN31_0)&(1<<LEFT_INDEX))>>(LEFT_INDEX);
 }
 
 // return current state of up switch
 uint32_t UP_Switch_In(void){
     // write this
-    return (GPIOB->DIN31_0)&UP_INDEX;
+    return ((GPIOB->DIN31_0)&(1<<UP_INDEX))>>(UP_INDEX);
 }
 
 // return current state of right switch
 uint32_t RIGHT_Switch_In(void){
     // write this
-    return (GPIOB->DIN31_0)&RIGHT_INDEX;
+    return ((GPIOB->DIN31_0)&(1<<RIGHT_INDEX))>>(RIGHT_INDEX);
 }
 
 // return current state of down switch
 uint32_t DOWN_Switch_In(void){
     // write this
-    return (GPIOB->DIN31_0)&DOWN_INDEX;
+    return ((GPIOB->DIN31_0)&(1<<DOWN_INDEX))>>(DOWN_INDEX);
 }
 
 // return all switch current values
+// Joystick bit 4, left bit 3, up bit 2, right bit 1, down bit 0
 uint32_t Switch_In(void){
-    return JOYSTICK_Switch_In() | LEFT_Switch_In() | UP_Switch_In() | DOWN_Switch_In() | RIGHT_Switch_In();
+    return (JOYSTICK_Switch_In()<<4 | LEFT_Switch_In()<<3 | UP_Switch_In()<<2 | RIGHT_Switch_In()<<1 | DOWN_Switch_In());
 }
