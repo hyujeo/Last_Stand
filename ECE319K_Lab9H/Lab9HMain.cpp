8d11570dc9590a92f0824a68b1383ce306a8b050
JonathanSimon098 <jonedu1130@gmail.com>
2024-04-22 23:58:09 -0500

    Start and Gameover menu

--- ECE319K_Lab9H/Lab9HMain.cpp
+++ ECE319K_Lab9H/Lab9HMain.cpp
@@ -63,8 +63,10 @@ uint32_t ADC0_ypos;
 int32_t slidepot_distance;
 uint32_t button_inputs;
 int refresh = 0;
-int last = 0;
+int forced = 0;
+int last_shoot, last_nav, go_game = 0;
 int language = 0; // 0 is english, 1 is spanish
+int end_selection = 0;
 int current_screen = 0; // 0 - welcome screen ; 1 - main game; 2 - Game Over
 
 // games  engine runs at 30Hz
@@ -82,18 +84,31 @@ void TIMG12_IRQHandler(void){
         slidepot_distance = 2000;
     }
     // 2) read input switches
-    Switches.All_Switch_In();
+    uint8_t all_switches = Switches.All_Switch_In();
     JoyStick_In(&ADC0_xpos, &ADC0_ypos);
     uint8_t shoot_btn_input = Switches.DOWN_Switch_In();
+    uint8_t nav_right_input = Switches.RIGHT_Switch_In();
     // 3) move sprites
     // 4) start sounds
     // determine which sound to play based on certain scenarious
-    if((last == 0) && (shoot_btn_input == 1) && (current_screen == 0)){
-        last = 1;
+    if((all_switches) && (current_screen == 0)){
+        go_game = 1;
+    }
+    if((last_shoot == 0) && (shoot_btn_input == 1) && (current_screen == 1)){
+        last_shoot = 1;
         Sound_Shoot(); // if shoot button was pressed
     }
-    if((last!=0)&&(shoot_btn_input==0)){ // high to low
-        last = 0;
+    if((last_shoot!=0)&&(shoot_btn_input==0)){ // high to low
+        last_shoot = 0;
+    }
+    if((last_nav == 0) && (end_selection == 1) && (current_screen == 2) && (nav_right_input == 1)){
+        last_nav = 1;
+        current_screen = 0;
+        forced = 1;
+        Sound_Ufo_Highpitch_Menu();
+    }
+    if((last_nav!=0)&&(nav_right_input==0)){ // high to low
+        last_nav = 0;
     }
     // 5) set semaphore
     refresh = 1;
@@ -122,37 +137,57 @@ const char Directions_English[] ="-Press Any To Begin-";
 const char Directions_Spanish[] = "-Toque Para Empezar-";
 const char Sel_Language_English[]="Language:";
 const char Sel_Language_Spanish[]="Lenguaje:";
-const char Language_English[]=" English";
-const char Language_Spanish[]=" Espa\xA4ol";
-const char Goodbye_Portuguese[] = "Tchau";
-const char Goodbye_French[] = "Au revoir";
-const char Language_Portuguese[]="Portugu\x88s";
-const char Language_French[]="Fran\x87" "ais";
-const char *Phrases[2][4]={
-  {Title_English,Directions_English,Sel_Language_English,Language_English}, // row 1 English
-  {Title_Spanish,Directions_Spanish,Sel_Language_Spanish,Language_Spanish}, // row 2 Spanish
+const char Language_English[]="English";
+const char Language_Spanish[]="Espa\xA4ol";
+const char GameOver_English[] = "Game Over";
+const char GameOver_Spanish[] = "Terminaste";
+const char Score_English[]="Score";
+const char Score_Spanish[]="Puntos";
+const char Restart_English[]="Restart";
+const char Restart_Spanish[]="Otra";
+const char Menu_English[]="Menu"; // same in spanish lol
+const char *Phrases[2][7]={
+  {Title_English, Directions_English, Sel_Language_English, Language_English, GameOver_English, Score_English, Restart_English}, // row 1 English
+  {Title_Spanish, Directions_Spanish, Sel_Language_Spanish, Language_Spanish, GameOver_Spanish, Score_Spanish, Restart_Spanish}, // row 2 Spanish
 };
 
 void Welcome_Screen(){
-    //backgrounds.init(NUM_BACKGROUND);
-    //backgrounds.draw('B');
     ST7735_FillScreen(ST7735_BLACK);
     ST7735_DrawString(1, 2, (Phrases[language][0]), ST7735_RED, ST7735_BLACK, 1); // title
     ST7735_DrawString(1, 6, (Phrases[language][2]), ST7735_GREEN, ST7735_BLACK, 1); // language header
     ST7735_DrawString(1, 12, (Phrases[language][1]), ST7735_WHITE, ST7735_BLACK, 1); // directions
 
     if(language == 0){
+        ST7735_FillRect(14, 78, 50, 12, ST7735_YELLOW);
         ST7735_DrawString(3, 8, (Phrases[0][3]), ST7735_BLACK, ST7735_YELLOW, 1); // language
-        ST7735_DrawString(3, 9, (Phrases[1][3]), ST7735_WHITE, ST7735_BLACK, 1); // language
+        ST7735_DrawString(3, 10, (Phrases[1][3]), ST7735_WHITE, ST7735_BLACK, 1); // language
     }else{
+        ST7735_FillRect(14, 98, 50, 12, ST7735_YELLOW);
         ST7735_DrawString(3, 8, (Phrases[0][3]), ST7735_WHITE, ST7735_BLACK, 1); // language
-        ST7735_DrawString(3, 9, (Phrases[1][3]), ST7735_BLACK, ST7735_YELLOW, 1); // language
+        ST7735_DrawString(3, 10, (Phrases[1][3]), ST7735_BLACK, ST7735_YELLOW, 1); // language
     }
+}
 
+void Game_Over(){
+    ST7735_FillScreen(ST7735_BLACK);
+    ST7735_DrawString(5, 2, (Phrases[language][4]), ST7735_RED, ST7735_BLACK, 1); // game over header
+    ST7735_SetCursor(6,7);
+    printf("%s: %d", (Phrases[language][5]), score);
+
+    if(end_selection == 0){
+        ST7735_FillRect(28, 98, 46, 12, ST7735_WHITE);
+        ST7735_DrawString(5, 10, (Phrases[language][6]), ST7735_BLACK, ST7735_WHITE, 1); // restart
+        ST7735_DrawString(5, 12, Menu_English, ST7735_WHITE, ST7735_BLACK, 1); // menu
+    }else{
+        ST7735_FillRect(28, 118, 32, 12, ST7735_WHITE);
+        ST7735_DrawString(5, 10, (Phrases[language][6]), ST7735_WHITE, ST7735_BLACK, 1); // restart
+        ST7735_DrawString(5, 12, Menu_English, ST7735_BLACK, ST7735_WHITE, 1); // menu
+    }
 
 }
 
 void Game_Init() {
+  ST7735_FillScreen(ST7735_BLACK);
   player.push(PLAYER_X >> 8, PLAYER_Y >> 8, 0, 0, 0); // initialize player
   backgrounds.init(NUM_BACKGROUND);
   blackRectangles.init(NUM_OBJECTS);
@@ -257,6 +292,7 @@ int main(void){ // final main
   //TimerG0_IntArm(13333,100,2); //
   // initialize all data structures
   //Game_Init();
+  //Game_Over();
   Welcome_Screen();
   __enable_irq();
   while(1){
@@ -275,11 +311,31 @@ int main(void){ // final main
               if((ADC0_ypos > 2500) && (language == 1)){
                   language = 0;
                   Welcome_Screen();
-                  Sound_Ufo_Highpitch_Menu();
+                  //Sound_Ufo_Highpitch_Menu();
               }else if((ADC0_ypos < 2000) && (ADC0_ypos > 0) && (language == 0)){
                   language = 1;
                   Welcome_Screen();
-                  Sound_Ufo_Highpitch_Menu();
+                  //Sound_Ufo_Highpitch_Menu();
+              }else if(forced == 1){
+                  Welcome_Screen();
+                  forced = 0;
+              }
+              if(go_game){
+                  Game_Init();
+                  go_game = 0;
+                  current_screen = 1;
+              }
+          }else if(current_screen == 1){ // game screen
+
+          }else if(current_screen == 2){ // game over screen
+              if((ADC0_ypos > 2500) && (end_selection == 1)){
+                  end_selection = 0;
+                  Game_Over();
+                  //Sound_Ufo_Highpitch_Menu();
+              }else if((ADC0_ypos < 2000) && (ADC0_ypos > 0) && (end_selection == 0)){
+                  end_selection = 1;
+                  Game_Over();
+                  //Sound_Ufo_Highpitch_Menu();
               }
           }
 
